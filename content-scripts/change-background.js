3bde9c72962a95b34b925ea8c96cbc9d619009f3
7azmi <a7.h3.04@gmail.com>
2025-05-16 19:12:58 +0800

    easier than I thought

---
+++ content-scripts/change-background.js
@@ -0,0 +1,8 @@
+// This script runs in the context of example.com
+
+console.log("My background changer extension script started!");
+
+// Find the body element and change its background color
+document.body.style.backgroundColor = 'lightblue';
+
+console.log("Background color should now be lightblue.");
